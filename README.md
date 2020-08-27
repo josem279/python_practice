2f353629329767286fd7c10546c2eb36cc1576ae
Jose Martinez <martinez.jose279@gmail.com>
2020-08-27 16:52:55 -0400

    Updated README.md file and added work on reading and writing to files.

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 ## This repository contains examples of my work while practicing the fundamentals of Python!
 
-## Overview:
+## Overview
 
 - The "python_strings_numbers.py" file demonstrates the simplest forms of storing strings and numbers as variables, manipulating numbers and strings, creating and using f-strings, and examples of arithmetic operators.
 - The "lists_tuples_sets_dicts.py" file demonstrates basics on working with lists, the unique properties of tuples, and touches on how we can create and work with dictionaries.
-- The "if_elif_else_and_loops.py" file goes over how to set up conditions using conditional operators and booleans and how to incorporate them into loops. Furthermore, the file goes over the basics on how to create "for" and "while" loops as well as some best practices.
+- The "if_elif_else_and_loops.py" file goes over how to set up conditions using conditional operators and booleans and how to incorporate them into loops. Furthermore, the file goes over the basics on how to create "for" and "while" loops as well as some best practices
 - The "functions.py" file demonstrates basics when creating and calling functions and lambdas.
 - The modules folder contains examples on how to import modules, calling functions from modules, and shows examples of built in modules from the Python library. Additionally, the "os_module.py" file goes into further detail on the os module and some of the useful methods found in that module.
+- The read_write_to_files folder contains work detailing best practice one should follow when opening files in scripts. The "read_write_to_files.py" file also demonstrates how we can read information from text files, how to write text, and how to iterate through certain lines when reading in data or writing data to another file.
--- read_write_to_files/read_write_to_files.py
+++ read_write_to_files/read_write_to_files.py
@@ -4,7 +4,55 @@
 import os
 
 print("Current directory is", os.getcwd())
-# Opening a file as read only
-#file_to_open = os.path.join("example.txt")
+# Creating variables for the files that we want to open/create
+file_to_open = os.path.join("read_write_to_files", "example.txt")
+file_to_write = os.path.join("read_write_to_files", "example2.txt")
+
+print(file_to_open)
+# Opening file with context manager so that we do not have to explicitly close the file
+# throwing in r as mode to indicate read only and then reading in all contents of a file
+with open(file_to_open, mode= "r") as testing_file:
+    # testing_file_contents = testing_file.read()
+    # print(testing_file_contents)
+
+
+# If we do not want to read in all data we can also read in one line at a time
+    # testing_file_contents = testing_file.readline()
+    # print(testing_file_contents, end="")
+
+    
+# To prevent running out of memory or prevent using same line of code over and over, we can
+# Use a for loop to print out each line in a file
+    # for line in testing_file:
+    #     print(line, end="")
+
+
+# When working with big data sets we can use the read() method to print out data by
+# a certain number of characters
+    size_to_read = 100
+    testing_file_contents = testing_file.read(size_to_read)
+    while len(testing_file_contents) > 0:
+        print(testing_file_contents, end="")
+        testing_file_contents = testing_file.read(size_to_read)
+
+
+# Writing to files
+# We use the same context manager but indicate mode as "w"
+# This file does not necessarily have to exist. If it is in "w" mode then file will be created
+with open(file_to_write, mode= "w") as w_file:
+    # Writing text to the file
+    w_file.write("Test writing")
+    # Adding in more than one write function to file add text to end point of previous write function
+    w_file.write("Test writing")
+
+
+
+# Creating a copy of read only text file by writing each line to a new "example_copy.txt" file
+with open(file_to_open, "r") as rf:
+    with open("example_copy.txt", "w") as wf:
+        for line in rf:
+            wf.write(line)
+
+
 
 
